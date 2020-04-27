dce9648ad4b61b0b2f1b8348def79d0b35749285
yattom <tsutomu.yasui@gmail.com>
2020-04-27 20:21:13 +0900

    cosmetic

--- tests/e2e/helper.py
+++ tests/e2e/helper.py
@@ -1,10 +1,10 @@
 def compo_pos(browser, component):
-    '''
+    """
     return position of the component relative to table
     :param browser: WebDriver
     :param component: component to get position
     :return: { "top": Number, "left": Number }
-    '''
+    """
     table = browser.find_element_by_css_selector("div.table")
     table_loc = table.location
     comp_loc = component.location
