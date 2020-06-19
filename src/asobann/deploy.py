6f58906b176a5e9c1d56ed9650729a75ffdbb413
yattom <tsutomu.yasui@gmail.com>
2020-06-19 11:48:58 +0900

    don't purge all on deployment

--- src/asobann/deploy.py
+++ src/asobann/deploy.py
@@ -8,10 +8,13 @@ import asobann
 app = asobann.create_app()
 
 
+def purge_all():
+    for d in [asobann.store.tables, asobann.store.components, asobann.store.kits]:
+        d.purge_all()
+
 if __name__ == '__main__':
     with open(Path(__file__).parent / "./initial_deploy_data.json", encoding='utf-8') as f:
         default_data = json.load(f)
-    for d in [asobann.store.tables, asobann.store.components, asobann.store.kits]:
-        d.purge_all()
+    # purge_all()
     asobann.store.components.store_default(default_data["components"])
     asobann.store.kits.store_default(default_data["kits"])
