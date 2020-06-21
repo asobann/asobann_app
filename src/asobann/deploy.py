90b60d278c2cdc77089813412ec6afdb7b1a7aeb
yattom <tsutomu.yasui@gmail.com>
2020-06-21 10:16:28 +0900

    purge data on deploy

--- src/asobann/deploy.py
+++ src/asobann/deploy.py
@@ -15,6 +15,6 @@ def purge_all():
 if __name__ == '__main__':
     with open(Path(__file__).parent / "./initial_deploy_data.json", encoding='utf-8') as f:
         default_data = json.load(f)
-    # purge_all()
+    purge_all()
     asobann.store.components.store_default(default_data["components"])
     asobann.store.kits.store_default(default_data["kits"])
