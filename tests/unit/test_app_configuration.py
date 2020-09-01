cdcff5e8dadb8cce70514d67a8dacd85fb5c1c5d
yattom <tsutomu.yasui@gmail.com>
2020-09-01 09:19:58 +0900

    refactor tests

--- tests/unit/test_app_configuration.py
+++ tests/unit/test_app_configuration.py
@@ -3,80 +3,103 @@ import os
 import asobann
 import pytest
 
-
 PARAMS = [
     {
         'id': 'development',
-        'FLASK_ENV': 'development',
-        'env': {
+        'input': {
+            'FLASK_ENV': 'development',
+            'env': {
+            },
         },
-        'config': {
-            'REDIS_URI': None,
-            'MONGO_URI': 'mongodb://localhost:27017/ex2dev',
-            'BASE_URL': 'http://localhost:5000',
-            'GOOGLE_ANALYTICS_ID': None,
+        'expected': {
+            'config': {
+                'REDIS_URI': None,
+                'MONGO_URI': 'mongodb://localhost:27017/ex2dev',
+                'BASE_URL': 'http://localhost:5000',
+                'GOOGLE_ANALYTICS_ID': None,
+            },
         },
     },
     {
         'id': 'development with PUBLIC_HOSTNAME (dev on AWS)',
-        'FLASK_ENV': 'development',
-        'env': {
-            'PUBLIC_HOSTNAME': 'asobann.example.com',
+        'input': {
+            'FLASK_ENV': 'development',
+            'env': {
+                'PUBLIC_HOSTNAME': 'asobann.example.com',
+            },
         },
-        'config': {
-            'BASE_URL': 'https://asobann.example.com',
+        'expected': {
+            'config': {
+                'BASE_URL': 'https://asobann.example.com',
+            },
         },
     },
     {
         'id': 'development (Google Analytics is disabled)',
-        'FLASK_ENV': 'development',
-        'env': {
-            'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+        'input': {
+            'FLASK_ENV': 'development',
+            'env': {
+                'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+            },
         },
-        'config': {
-            'GOOGLE_ANALYTICS_ID': None,
+        'expected': {
+            'config': {
+                'GOOGLE_ANALYTICS_ID': None,
+            },
         },
     },
     {
         'id': 'test',
-        'FLASK_ENV': 'test',
-        'env': {
+        'input': {
+            'FLASK_ENV': 'test',
+            'env': {
+            },
+            'testing': True,
         },
-        'config': {
-            'REDIS_URI': None,
-            'MONGO_URI': 'mongodb://localhost:27017/ex2test',
-            'BASE_URL': '*',
-            'GOOGLE_ANALYTICS_ID': None,
+        'expected': {
+            'config': {
+                'REDIS_URI': None,
+                'MONGO_URI': 'mongodb://localhost:27017/ex2test',
+                'BASE_URL': '*',
+                'GOOGLE_ANALYTICS_ID': None,
+            },
         },
-        'testing': True,
     },
     {
         'id': 'production',
-        'FLASK_ENV': 'production',
-        'env': {
-            'REDIS_URI': 'redis://example.com',
-            'MONGODB_URI': 'mongodb://example.com/',
-            'PUBLIC_HOSTNAME': 'asobann.example.com',
-            'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+        'input': {
+            'FLASK_ENV': 'production',
+            'env': {
+                'REDIS_URI': 'redis://example.com',
+                'MONGODB_URI': 'mongodb://example.com/',
+                'PUBLIC_HOSTNAME': 'asobann.example.com',
+                'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+            },
         },
-        'config': {
-            'REDIS_URI': 'redis://example.com',
-            'MONGO_URI': 'mongodb://example.com/?retryWrites=false',
-            'BASE_URL': 'https://asobann.example.com',
-            'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+        'expected': {
+            'config': {
+                'REDIS_URI': 'redis://example.com',
+                'MONGO_URI': 'mongodb://example.com/?retryWrites=false',
+                'BASE_URL': 'https://asobann.example.com',
+                'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+            },
         },
     },
     {
         'id': 'production (preceding period in PUBLIC_HOSTNAME)',
-        'FLASK_ENV': 'production',
-        'env': {
-            'REDIS_URI': 'redis://example.com',
-            'MONGODB_URI': 'mongodb://example.com/',
-            'PUBLIC_HOSTNAME': '.asobann.example.com',
-            'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+        'input': {
+            'FLASK_ENV': 'production',
+            'env': {
+                'REDIS_URI': 'redis://example.com',
+                'MONGODB_URI': 'mongodb://example.com/',
+                'PUBLIC_HOSTNAME': '.asobann.example.com',
+                'GOOGLE_ANALYTICS_ID': 'UA-DUMMYID',
+            },
         },
-        'config': {
-            'BASE_URL': 'https://asobann.example.com',
+        'expected': {
+            'config': {
+                'BASE_URL': 'https://asobann.example.com',
+            },
         },
     },
 ]
@@ -84,14 +107,16 @@ PARAMS = [
 
 @pytest.mark.parametrize('param', PARAMS, ids=[p['id'] for p in PARAMS])
 def test_config(param):
-    os.environ['FLASK_ENV'] = param['FLASK_ENV']
+    input_ = param['input']
+    expected = param['expected']
+    os.environ['FLASK_ENV'] = input_['FLASK_ENV']
     try:
-        for key, value in param['env'].items():
+        for key, value in input_['env'].items():
             os.environ[key] = value
         app = Flask(__name__)
-        asobann.configure_app(app, testing=param.get('testing', False))
-        for key, value in param['config'].items():
+        asobann.configure_app(app, testing=input_.get('testing', False))
+        for key, value in expected['config'].items():
             assert app.config[key] == value
     finally:
-        for key, value in param['env'].items():
+        for key, value in input_['env'].items():
             del os.environ[key]
