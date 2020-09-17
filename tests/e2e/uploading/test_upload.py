77354954267688e4fc74974edc7b5c2a2eed57e1
yattom <tsutomu.yasui@gmail.com>
2020-09-17 13:33:35 +0900

    add e2e test for uploading image

--- tests/e2e/uploading/test_upload.py
+++ tests/e2e/uploading/test_upload.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from ..helper import GameHelper, TOP
+
+
+def upload_kit(host, filename, image_filenames=[]):
+    host.menu.open_toolbox.execute()
+    host.toolbox.use(host.toolbox.upload_kit)
+    host.toolbox.upload_kit.select_json_file(str(Path(__file__).parent / filename))
+    host.toolbox.upload_kit.upload()
+    host.toolbox.upload_kit.accept_success_alert()
 
 
 @pytest.mark.usefixtures("server")
@@ -10,17 +17,12 @@ from ..helper import GameHelper, TOP
 class TestUploadKit:
     def test_success(self, browser: webdriver.Firefox):
         host = GameHelper.player(browser)
-
-        host.menu.open_toolbox.execute()
-        host.toolbox.use(host.toolbox.upload_kit)
-        host.toolbox.upload_kit.select_json_file(str(Path(__file__).parent / 'test_menu_kit.json'))
-        host.toolbox.upload_kit.upload()
-        host.toolbox.upload_kit.accept_success_alert()
+        upload_kit(host, 'test_menu_kit.json')
 
         host.menu.add_kit.execute()
         host.menu.should_see_kit("Test Kit")
 
-    def test_cancel(self, browser: webdriver.Firefox, default_kits_and_components):
+    def test_cancel(self, browser: webdriver.Firefox):
         host = GameHelper.player(browser)
 
         host.menu.open_toolbox.execute()
@@ -30,7 +32,7 @@ class TestUploadKit:
         host.menu.add_kit.execute()
         host.menu.should_not_see_kit("Test Kit")
 
-    def test_no_jsonfile(self, browser: webdriver.Firefox, default_kits_and_components):
+    def test_no_jsonfile(self, browser: webdriver.Firefox):
         host = GameHelper.player(browser)
 
         host.menu.open_toolbox.execute()
@@ -40,3 +42,14 @@ class TestUploadKit:
 
         host.menu.add_kit.execute()
         host.menu.should_not_see_kit("Test Kit")
+
+    class TestUploadImage:
+        def test_one_image(self, browser: webdriver.Firefox):
+            host = GameHelper.player(browser)
+            upload_kit(host, 'test_menu_kit_with_one_image.json', ['example.png'])
+
+            host.menu.add_kit.execute()
+            host.menu.add_kit_from_list("Test Kit")
+            host.menu.add_kit_done()
+
+            assert 'image_url: hoge' in host.component_by_name('Test Component').face()
