c99e3e789bfc9eb6a2103872340834b39b6be046
yattom <tsutomu.yasui@gmail.com>
2020-09-27 14:18:14 +0900

    modify Dockerfile to faster build and push

--- src/asobann/__init__.py
+++ src/asobann/__init__.py
@@ -1,12 +1,9 @@
 # from gevent import monkey
 # monkey.patch_all()
 import eventlet
-eventlet.monkey_patch()
 
+eventlet.monkey_patch()
 
 from flask_socketio import SocketIO, emit, join_room
 
-
 socketio = SocketIO()
-
-
