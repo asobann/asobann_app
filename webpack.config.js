1b3fa666714189ba74c7d2dee022abfa881426c9
yattom <tsutomu.yasui@gmail.com>
2020-10-05 22:47:29 +0900

    add source map

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/js/play_session.js',
+  devtool: 'eval-source-map',
   output: {
     path: path.resolve('./src/asobann/app/static'),
     filename: 'main.js',
