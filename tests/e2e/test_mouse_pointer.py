300cc2631cf32626d3c559279ddea498c4ce56f7
yattom <tsutomu.yasui@gmail.com>
2021-05-16 11:31:43 +0900

    fix unfinished thread
    The thread live forever trying to already closed browser.
    It showed no error before but it's wrong anyway.

--- tests/e2e/test_mouse_pointer.py
+++ tests/e2e/test_mouse_pointer.py
@@ -11,14 +11,19 @@ def test_observers_pointer_should_not_be_seen(debug_handler_wait, server, browse
     host.should_have_text("you are host")
 
     from threading import Thread
+    running = True
 
     def move_mouse():
-        while True:
+        while running:
             another.move_mouse_by_offset((30, 30))
             another.move_mouse_by_offset((-30, -30))
 
     another.move_mouse_by_offset((500, 200))
     another.go(host.current_url)
-    Thread(target=move_mouse).start()
+    thread = Thread(target=move_mouse)
+    thread.start()
 
     host.should_not_have_text('nobody', timeout=5)
+
+    running = False
+    thread.join()
