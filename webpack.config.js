d552184891fef7ca5332759e0e7cc0de4301b84a
yattom <tsutomu.yasui@gmail.com>
2021-08-15 18:49:47 +0900

    use aws-amplify npm module for auth with cognito

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './src/js/play_session.js',
-    traces: './src/js/debug/traces.js',
-  },
-  devtool: 'eval-source-map',
-  output: {
-    path: path.resolve('./src/asobann/app/static'),
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
+    entry: {
+        main: './src/js/play_session.js',
+        traces: './src/js/debug/traces.js',
+    },
+    devtool: 'eval-source-map',
+    output: {
+        path: path.resolve('./src/asobann/app/static'),
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset/resource',
+            },
+            {
+                // to avoid errors from graphql with webpack 5
+                // see https://github.com/graphql/graphql-js/issues/2721#issuecomment-723008284
+                test: /\.m?js/,
+                resolve: {
+                    fullySpecified: false
+                }
+            },
         ],
-      },
-      {
-        test: /\.(png|jpg)$/,
-        type: 'asset/resource',
-      },
-    ],
-  },
+    },
 };
